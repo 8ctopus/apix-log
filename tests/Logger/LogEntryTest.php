18a3d0860104a22b95aa0552f203ccc6bbab5b38
8ctopus <hello@octopuslabs.io>
2023-02-13 12:19:17 +0400

    added: log entry test

---
+++ tests/Logger/LogEntryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Apix Project.
+ *
+ * (c) Franck Cassedanne <franck at ouarz.net>
+ *
+ * @license http://opensource.org/licenses/BSD-3-Clause  New BSD License
+ */
+
+namespace Apix\Log\tests\Logger;
+
+use Apix\Log\LogEntry;
+use Apix\Log\LogFormatter;
+
+/**
+ * @internal
+ *
+ * @covers Apix\Log\LogEntry
+ */
+final class LogEntryTest extends \PHPUnit\Framework\TestCase
+{
+    public function testConstructor() : void
+    {
+        $entry = new LogEntry('emergency', 'test', ['context']);
+        $entry->setFormatter(new LogFormatter());
+
+        static::assertSame(date("[Y-m-d H:i:s]") . ' EMERGENCY test', (string) $entry);
+
+        $entry = new LogEntry(0, 'test', ['context']);
+        $entry->setFormatter(new LogFormatter());
+
+        static::assertSame(date("[Y-m-d H:i:s]") . ' EMERGENCY test', (string) $entry);
+    }
+}
