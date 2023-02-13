b73fe25b54a1484d5743efaf3a9dc93136b96a78
8ctopus <hello@octopuslabs.io>
2023-02-13 11:04:20 +0400

    cleanup: code

--- src/Logger/File.php
+++ src/Logger/File.php
@@ -28,17 +28,12 @@ class File extends ErrorLog
      */
     public function __construct(string $file)
     {
-        if (null === $file || !file_exists($file) && !touch($file)) {
-            throw new InvalidArgumentException(
-                sprintf('Log file "%s" cannot be created', $file),
-                1
-            );
+        if (!file_exists($file) && !touch($file)) {
+            throw new InvalidArgumentException(sprintf('Log file "%s" cannot be created', $file), 1);
         }
+
         if (!is_writable($file)) {
-            throw new InvalidArgumentException(
-                sprintf('Log file "%s" is not writable', $file),
-                2
-            );
+            throw new InvalidArgumentException(sprintf('Log file "%s" is not writable', $file), 2);
         }
 
         $this->destination = $file;
